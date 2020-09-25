e8ddffa6c93f18dccaedeff75e91d30c055a798b
A. Zeng <admin@admins-mbp.lan>
2020-09-25 00:27:50 -0700

    first commit

---
+++ MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by admin on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
